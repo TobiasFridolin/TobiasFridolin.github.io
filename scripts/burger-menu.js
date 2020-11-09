f6abc60f379a2d5893e7cd7f6118bdd9f1bddc66
Tobias Fridolin <tobiasfridolin@gmail.com>
2020-11-09 19:03:36 +0100

    CV

--- scripts/burger-menu.js
+++ scripts/burger-menu.js
@@ -6,6 +6,8 @@ document.querySelector('.nav__burger--collabsed').addEventListener('click', even
 
 document.querySelector('.nav__burger--open').addEventListener('click', event =>{
     document.querySelector('.burger-menu').style.transform = "translateY(-100%)";
+
+    
     document.querySelector('.nav__burger--open').style.display = "none";
     document.querySelector('.nav__burger--collabsed').style.display = "block";
 })
