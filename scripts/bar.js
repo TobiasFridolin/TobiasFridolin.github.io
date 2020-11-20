ce76c4e9a71fd342502b799da7b845508b9da54f
Tobias Fridolin <tobiasfridolin@gmail.com>
2020-11-20 13:31:36 +0100

    Portfolio
    - Bar added
    - Portfolio overview, pictures are showed first in mobile
    - Outline shown in mobile
    - About Cases button has shadow
    - Text adjustments

---
+++ scripts/bar.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    var scrollPrev;
+    
+    function addScrollBar(){
+        $(".bar").addClass("bar--scrolling");
+    }
+    function removeScrollBar(){
+        $(".bar").removeClass("bar--scrolling");
+    }
+
+    function handleScroll(){
+
+        return function(){
+            if(window.scrollY > $(".hero").innerHeight()){
+                window.scrollY > scrollPrev ? addScrollBar() : removeScrollBar();
+                $(".bar").removeClass("bar--transparent");
+            }else{
+                $(".bar").addClass("bar--transparent");
+            }
+            scrollPrev = window.scrollY;
+        }
+    }
+
+
+    $(document).scroll(handleScroll());
+ 
+});
